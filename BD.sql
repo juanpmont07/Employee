302290ebade2524382063eb99f769d5ca6a3b483
juanpmont07 <44062843+juanpmont07@users.noreply.github.com>
2020-10-27 18:26:32 -0600

    Update BD.sql

--- BD.sql
+++ BD.sql
@@ -1,8 +1,12 @@
+--Se requiere ejecutar por bloques
 
+--Ejecutar Primero
 create database employee
+
+--Segundo
 use employee
 
-
+--Tercero
 create table employee(
  id varchar(20) primary key,
  name varchar(30) ,
@@ -12,5 +16,11 @@ create table employee(
  id_boss varchar(20) foreign key (id_boss) references employee (id)
 )
 
+--Cuarto
+insert into employee
+values ('101010','Juan','Castro','350376544','jp@gmail.com','101010')
+
+insert into employee
+values ('111112','Jose','Montoya','344157874','jose@gmail.com','101010')
 
 
